96616cc4e037f510ec4f16be1bcc41a6ff96326d
kburchfiel <32693822+kburchfiel@users.noreply.github.com>
2019-10-27 22:05:45 +0000

    Add project files.

---
+++ UTWinLose/UTWinLose.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+int main()
+{
+
+	int texaswin = 2;
+	int ouwin = 2;
+	std::cout << "Did Texas win? If yes, type 1; if no, write 0 :\n";
+	std::cin >> texaswin;
+	std::cout << "Did OU win? If yes, type 1; if no, write 0 :\n";
+	std::cin >> ouwin;
+
+	if (texaswin == 1 or ouwin == 0) {
+		std::cout << "Texas wins!\n";
+	}
+	else {
+		std::cout << "Texas loses :(\n";
+	}
+	std::cin.get();
+	std::cin.get();
+	}
+
